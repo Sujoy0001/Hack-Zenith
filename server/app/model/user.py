8298ebd030a83d7122e87c9a11dd02377de9feb7
SUJOY GARAI <sujoycode999@gmail.com>
2025-12-26 21:13:10 +0530

    refactor User model and add user router

--- server/app/model/user.py
+++ server/app/model/user.py
@@ -4,22 +4,7 @@ class User(BaseModel):
     id: int
     name: str
     email: EmailStr
-    is_active: bool = True      
-    is_superuser: bool = False
+    is_active: bool = True     
     is_verified: bool = False
     created_at: str
     updated_at: str
-    class Config:
-        orm_mode = True
-    schema_extra = {
-        "example": {
-            "id": 1,
-            "name": "John Doe",
-            "email": "john.doe@example.com",
-            "is_active": True,
-            "is_superuser": False,
-            "is_verified": True,
-            "created_at": "2023-01-01T12:00:00Z",
-            "updated_at": "2023-01-02T12:00:00Z"
-        }
-    }
