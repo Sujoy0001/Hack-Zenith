9235073e24729cec9c218a8d24444ca5a79a5df9
SUJOY GARAI <160879898+Sujoy0001@users.noreply.github.com>
2026-01-02 14:17:19 +0530

    Delete server/app directory

--- server/app/db/mongodb.py
+++
@@ -1,8 +0,0 @@
-from motor.motor_asyncio import AsyncIOMotorClient
-from config import MONGODB_URL, DATABASE_NAME
-
-client = AsyncIOMotorClient(MONGODB_URL)
-database = client[DATABASE_NAME]
-
-def get_database():
-    return database
